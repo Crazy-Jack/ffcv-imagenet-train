20d93517ea4e11f053a6518b7c1705767192e693
ylz1122 <ylz1122@athena.athena.research.northwestern.edu>
2023-08-09 14:45:00 -0500

    typo

--- scripts/alexnet/run_pod_test.sh
+++ scripts/alexnet/run_pod_test.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES=0,1;
 
 # name="vit-b-configs/vit_b_32_32_epochs_1_gpu_varyingsize_0_base_0";
-name="alexnet_configs/alexnet_5layers_32_epochs_test.yaml";
+name="alexnet_configs/alexnet_5layers_32_epochs_test";
 # name="vit-b-configs/vit_b_16_epochs_dist_gpu_varyingsize_0_topk_1";
 # name="vit-b-configs/vit_b_16_epochs_1_gpu_varyingsize_0_topk_1";
 # name="vit-b-configs/vit_b_16_epochs_1_gpu_varyingsize_1";
